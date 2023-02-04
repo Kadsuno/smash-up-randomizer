a020bf683f60b27baaa7b6a1ee3d0d2fb68bf0c4
Mike Rauch <mike.rauch@proton.me>
2023-02-04 02:44:38 +0100

    Modifed the form to send the data to the controller. Added a popup if the deck entry already exists

--- resources/views/backend/decks-manager.blade.php
+++ resources/views/backend/decks-manager.blade.php
@@ -14,14 +14,14 @@
             </h2>
             <ol class="list-group list-group-numbered">
                 @foreach ($decks as $deck)
-                    <li class="list-group-item d-flex justify-content-between align-items-start">
-                        <div class="ms-2 me-auto">
+                <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
                         <div class="fw-bold">{{ $deck->name }}</div>
-                        </div>
-                        <a class="btn btn-sm btn-primary rounded-pill" href="#">Delete</a>
-                    </li>
+                    </div>
+                    <a class="btn btn-sm btn-primary rounded-pill" href="#">Delete</a>
+                </li>
                 @endforeach
-              </ol>
+            </ol>
         </div>
     </div>
     <div class="container-fluid">
@@ -30,20 +30,23 @@
                 Add new decks
             </h2>
             <div>
-                <form>
+                <form method="GET" action="{{ route('decks-manager') }}">
                     <div class="row mb-2">
-                        <div class="col-6">
-                            <label for="deckName" class="form-label">Deck name</label>
-                            <input type="text" class="form-control" id="deckName">
+                        <label for="deckName" class="form-label">Deck name</label>
+                        <div class="col-4">
+                            <input type="text" class="form-control bg-white" name="deckName">
+                        </div>
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary">Save</button>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-2">
-                            <button type="submit" class="btn btn-primary">Save</button>    
-                        </div>
-                    </div>
-                  </form>
+                </form>
             </div>
         </div>
     </div>
+    @if ($deckExists == 1)
+        <script>
+            window.alert("The deck already exists!");
+        </script>
+    @endif
 </x-layouts.backend.backendMain>
