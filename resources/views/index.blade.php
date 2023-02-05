99d70a4ce0bf7218a5f6290e321de83d2b4d8acd
Mike Rauch <mike.rauch@proton.me>
2023-02-06 00:09:13 +0100

    Modified the content of the start page.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,27 @@
-      <x-layouts.main>
-            
-      </x-layouts.main>          
+<x-layouts.main>
+      <div class="container-fluid">
+            <div class="row">
+                  <form class="needs-validation" method="GET" action="{{ route('shuffle-decks') }}" novalidate>
+                        <div class="row mb-2">
+                              <label for="numberOfPlayers" class="form-label">Number of players</label>
+                              <div class="col-4">
+                                    <div class="has-validation">
+                                          <select class="form-select" name="numberOfPlayers">
+                                                <option selected>Choose the number of players</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                          </select>                                          
+                                          <div class="invalid-feedback">
+                                                Please choose the number of players.
+                                          </div>
+                                    </div>
+                              </div>
+                              <div class="col-4">
+                                    <button type="submit" class="btn btn-primary">Shuffle</button>
+                              </div>
+                        </div>
+                  </form>
+            </div>
+      </div>
+</x-layouts.main>
