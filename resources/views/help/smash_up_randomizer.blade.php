039ee9c7bcb2d183e4a3db70e1e08f669080a69a
Mike Rauch <mike.rauch@proton.me>
2023-02-11 15:43:12 +0100

    Created help template for smash up randomizer.

---
+++ resources/views/help/smash_up_randomizer.blade.php
@@ -0,0 +1,17 @@
+<x-layouts.main>
+    <div class="container-fluid">
+        <div class="row mb-5">
+            <h1>
+                {{ __('frontend.help_howto_header') }}
+            </h1>
+        </div>
+        <div class="row">
+            <p>
+                {{ __('frontend.help_howto_body') }}
+            </p>
+            <p>
+                {{ __('frontend.help_howto_fun') }}
+            </p>
+        </div>
+    </div>
+</x-layouts.main>
