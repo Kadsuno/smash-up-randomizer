66bd46a489c15057faa6c779566846cf7026e843
Mike Rauch <mike.rauch@proton.me>
2023-02-11 15:41:01 +0100

    Added footer.

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,2 +1,22 @@
 <script src="{{ asset('js/app.js') }}"></script>
 <script src="{{ asset('js/main.js') }}"></script>
+<footer class="footer px-5 container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <hr>
+            </div>
+        </div>
+        <div class="row pb-3">
+            <div class="col-12 col-md-auto text-center text-md-left">
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a class="text-white nav-link" href="{{ route('imprint') }}">
+                            {{ __('frontend.imprint_header') }}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
