de1f69a617ed5c37a5d326e93c8b81bb4754d132
Mike Rauch <mike.rauch@proton.me>
2023-02-11 15:39:11 +0100

    Added translations.

---
+++ resources/lang/de/backend.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'dashboard_header' => 'Willkommen im Backend-Bereich!',
+    'dashboard_body' => 'Im Backend-Bereich können über den Deck Manager vorhandene Fraktionen angzeigt und gelöscht werden. Des Weiteren werden neue Fraktionen über den Decks Manager hinzugefügt.',
+    'current_decks' => 'Aktuelle Fraktionen',
+    'delete_decks' => 'Löschen',
+    'add_decks' => 'Neue Fraktion hinzufügen',
+    'deck_name' => 'Name der Fraktion',
+    'deck_name_invalid' => 'Bitte geben Sie einen gültigen Namen ein. Das Eingabefeld darf nicht leer sein.',
+    'save' => 'Speichern',
+    'logout' => 'Logout',
+];
