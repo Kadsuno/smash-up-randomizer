89997241039d5a5e8e672345945156675aa2500f
Mike Rauch <mike.rauch@proton.me>
2023-02-11 15:43:34 +0100

    Created help template for smash up.

---
+++ resources/views/help/smash_up.blade.php
@@ -0,0 +1,17 @@
+<x-layouts.main>
+    <div class="container-fluid">
+        <div class="row mb-5">
+            <h1>
+                {{ __('frontend.help_smashup_header') }}
+            </h1>
+        </div>
+        <div class="row">
+            <p>
+                {{ __('frontend.help_smashup_body') }}
+            </p>
+            <p>
+                {{ __('frontend.help_smashup_function') }}
+            </p>
+        </div>
+    </div>
+</x-layouts.main>
