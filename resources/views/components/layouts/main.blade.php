d84aaf7aee64594e79ecbe7481d7f1032d5c4900
Mike Rauch <mike.rauch@proton.me>
2023-10-26 23:25:38 +0200

    Modified main template

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.header />
-    <main class="flex flex-1 container max-w-7xl mx-auto px-5 lg:px-40 space-x-5 mb-5 mt-5">
+    <main class="flex flex-1 container-fluid max-w-7xl mx-auto px-5 lg:px-40 space-x-5 mb-5 mt-5">
         {{ $slot }}
     </main>
 <x-layouts.footer />
