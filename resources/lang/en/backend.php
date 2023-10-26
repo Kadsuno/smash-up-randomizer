38c7cbedac42e9305f0021c294f1d061d965a3ed
Mike Rauch <mike.rauch@proton.me>
2023-10-26 23:23:49 +0200

    Created translations for backend

---
+++ resources/lang/en/backend.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'dashboard_header' => 'Welcome in the SUR Backend!',
+    'dashboard_body' => 'In the SUR backend, existing factions can be displayed and deleted via the Deck Manager. Furthermore, new fractions can be added via the Decks Manager.',
+    'current_decks' => 'Current Factions',
+    'delete_decks' => 'Delete',
+    'add_decks' => 'Add a Faction',
+    'edit_deck' => 'Edit',
+    'deck_name' => 'Name of the faction',
+    'deck_name_invalid' => 'Please enter a valid name. The input field must not be empty.',
+    'headline_csv' => 'Import Factions via CSV File',
+    'deck_csv' => 'CSV file to import',
+    'deck_csv_invalid' => 'Please choose a file to import factions.',
+    'save' => 'Save',
+    'upload' => 'Upload',
+    'logout' => 'Logout',
+];
