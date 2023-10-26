f3e1c761fd811ce01126dfb59a03eb9239b151bc
Mike Rauch <mike.rauch@proton.me>
2023-10-26 23:26:45 +0200

    Added privacy policy template

---
+++ resources/views/components/layouts/bottom-navigation/privacyPolicy.blade.php
@@ -0,0 +1,115 @@
+<x-layouts.main>
+    <div class="container-fluid">
+        <div class="row mb-5">
+            <h1 class="mb-3">
+                {{ __('frontend.privacyPolicy_header') }}
+            </h1>
+            <p>
+                {{ __('frontend.privacyPolicy_teaser') }}
+            </p>
+        </div>
+        <div class="row mb-5">
+            <h2 class="mb-3">
+                {{ __('frontend.privacyPolicy_accessData_header') }}
+            </h2>
+            <p>
+                {{ __('frontend.privacyPolicy_accessData_body') }}
+            </p>
+            <ul class="ms-3">
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_accessData_list_1') }}
+                </li>
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_accessData_list_2') }}
+                </li>
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_accessData_list_3') }}
+                </li>
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_accessData_list_4') }}
+                </li>
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_accessData_list_5') }}
+                </li>
+            </ul>
+            <p>
+                {{ __('frontend.privacyPolicy_purpose_body') }}
+            </p>
+            <ul class="ms-3">
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_purpose_list_1') }}
+                </li>
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_purpose_list_2') }}
+                </li>
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_purpose_list_3') }}
+                </li>
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_purpose_list_4') }}
+                </li>
+            </ul>
+            <p>
+                {{ __('frontend.privacyPolicy_legalBasis_body') }}
+            </p>
+        </div>
+        <div class="row mb-5">
+            <h2 class="mb-3">
+                {{ __('frontend.privacyPolicy_dataTransfer_header') }}
+            </h2>
+            <p>
+                {{ __('frontend.privacyPolicy_dataTransfer_body') }}
+            </p>
+            <ul class="ms-3">
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_dataTransfer_list_1') }}
+                </li>
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_dataTransfer_list_2') }}
+                </li>
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_dataTransfer_list_3') }}
+                </li>
+            </ul>
+        </div>
+        <div class="row mb-5">
+            <h2 class="mb-3">
+                {{ __('frontend.privacyPolicy_cookies_header') }}
+            </h2>
+            <p>
+                {{ __('frontend.privacyPolicy_cookies_body_1') }}
+            </p>
+            <p>
+                {{ __('frontend.privacyPolicy_cookies_body_2') }}
+            </p>
+            <p>
+                {{ __('frontend.privacyPolicy_cookies_body_3') }}
+            </p>
+            <p>
+                {{ __('frontend.privacyPolicy_cookies_body_4') }}
+            </p>
+            <p>
+                {{ __('frontend.privacyPolicy_cookies_body_5') }}
+            </p>
+        </div>
+        <div class="row">
+            <h2 class="mb-3">
+                {{ __('frontend.privacyPolicy_dataSubject_header') }}
+            </h2>
+            <p>
+                {{ __('frontend.privacyPolicy_dataSubject_body') }}
+            </p>
+            <ul class="ms-3">
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_dataSubject_list_1') }}
+                </li>
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_dataSubject_list_2') }}
+                </li>
+                <li class="list-item">
+                    {{ __('frontend.privacyPolicy_dataSubject_list_3') }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</x-layouts.main>
